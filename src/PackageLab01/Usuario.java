68ae6ce93fade415b761ee89cf7c9985ea975492
rdcel <rdcel@LAPTOP-SOA1VQTT>
2021-10-13 09:30:40 -0500

    test1 prueba

---
+++ src/PackageLab01/Usuario.java
@@ -0,0 +1,23 @@
+
+package PackageLab01;
+
+
+
+public class Usuario {
+ public String Nombre, Apellido, Email;
+  String Contraseña;
+  int ID;
+ public TipoUsuario Usuario; // TipoUsuario: Nuevo/Antiguo
+ public int NTrans;
+ 
+ Billetera MiBilletera = new Billetera();
+ 
+ 
+ Usuario (){
+
+
+ 
+         }
+
+
+}
